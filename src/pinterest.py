fb7baf754f6486c6bfe5a6f391e923d95a169b06
artursal1 <salmanovartur@gmail.com>
2023-06-21 22:59:01 +0300

    Trained model, check with Google colab

---
+++ src/pinterest.py
@@ -0,0 +1,10 @@
+from src import PinterestScraper, PinterestConfig
+
+configs = PinterestConfig(search_keywords="Atatürk", # Search word
+                          file_lengths=200,     # total number of images to download (default = "100")
+                          image_quality="orig", # image quality (default = "orig")
+                          bookmarks="")         # next page data (default= "")
+
+PinterestScraper(configs).download_images("data/raw/pinterest") # download images directly (photos/atatürk, photos/web-scraping)
+
+print(PinterestScraper(configs).get_urls()) # get image links
