57fd5ba0c97b15e37ef06b2d7e70cc2edde4d81c
artursal1 <salmanovartur@gmail.com>
2023-06-26 23:14:02 +0300

    Train model in colab, evaluate locally

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,14 +1,19 @@
 import tensorflow as tf
 
-data_dir = 'data/raw/pinterest'
-report_path = 'output/report-5.txt'
-model_path = 'models/my_model'
+data_dir = '../data/raw/PetImages'
+report_path = '../output/report-5.txt'
+model_path = '../models/my_model'
 
 
 def evaluate():
-    test_df = tf.keras.utils.image_dataset_from_directory(
+    _, val_ds = tf.keras.utils.image_dataset_from_directory(
         data_dir,
-        image_size=(180, 180))
+        validation_split=0.2,
+        subset="both",
+        seed=1337,
+        image_size=(180, 180),
+        batch_size=10,
+    )
     model = tf.keras.models.load_model(model_path)
     metrics = [
         tf.metrics.BinaryAccuracy(),
@@ -16,11 +21,11 @@ def evaluate():
         tf.metrics.Recall()
     ]
     model.compile(metrics=metrics)
-    result = model.evaluate(test_df)
+    result = model.evaluate(val_ds)
     _, acc, precision, recall = result
 
     result_lines = [
-        f'Test data len: {len(test_df)}\n'
+        f'Test data len: {len(val_ds)}\n'
         f'Accuracy: {acc:.3f}, Precision: {precision:.3f}, Recall: {recall:.3f}'
     ]
     with open(report_path, 'w', encoding='utf-8') as f:
